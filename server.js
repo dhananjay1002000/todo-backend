3f88a3f77f62c3ffab6f6957b0584df83142041a
dhananjay1002000 <55107701+dhananjay1002000@users.noreply.github.com>
2023-11-07 16:56:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(
         saveUninitialized: true,
         cookie: {
             maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
-            secure: false,
+            secure: true,
           },
         store:  MongoStore.create({mongoUrl:process.env.SESSION_URI}),
     })
